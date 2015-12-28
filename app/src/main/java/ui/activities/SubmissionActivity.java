db36e958730ebde00c43186f49b0512c08abd697
ဝင်းထိုက်အောင် <winhtaikaung28@hotmail.com>
2015-12-28 13:43:22 +0630

    Font Chooser Dialog added

--- app/src/main/java/ui/activities/SubmissionActivity.java
+++ app/src/main/java/ui/activities/SubmissionActivity.java
@@ -1,15 +1,21 @@
 package ui.activities;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.widget.AppCompatRadioButton;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
 
 import com.myanmarhumanrights.R;
 
@@ -19,21 +25,79 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 public class SubmissionActivity extends AppCompatActivity {
 
     WebView wv_formviewer;
+    AlertDialog dialog_fontchooser;
+    AppCompatRadioButton rdo_unicode,rdo_zawgyi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+        /*CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/Padauk.ttf")
                 .setFontAttrId(R.attr.fontPath)
                 .build()
-        );
+        );*/
         setContentView(R.layout.activity_submission);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.black));
 
+       dialog_fontchooser = new AlertDialog.Builder(this)
+        .setTitle("FontChooser").
+        setView(R.layout.dialog_font_chooser).
+        setPositiveButton("OK", null).
+        setNegativeButton("Cancel", null).create();
+
+
+        dialog_fontchooser.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                rdo_unicode=(AppCompatRadioButton)  dialog_fontchooser.findViewById(R.id.rdo_unicode);
+                rdo_zawgyi=(AppCompatRadioButton)   dialog_fontchooser.findViewById(R.id.rdo_zawgyi);
+
+                rdo_unicode.setChecked(true);
+
+
+                rdo_unicode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if(buttonView.getId() == rdo_unicode.getId()){
+                            if (rdo_unicode.isChecked()) {
+                                rdo_zawgyi.setChecked(false);
+                                Shar
+                            }else {
+                                rdo_zawgyi.setChecked(true);
+                            }
+                        }
+
+                    }
+                });
+                rdo_zawgyi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if(buttonView.getId() == rdo_zawgyi.getId()){
+                            if (rdo_zawgyi.isChecked()) {
+                                rdo_unicode.setChecked(false);
+                            }else {
+                                rdo_unicode.setChecked(true);
+                            }
+                        }
+                    }
+                });
+            }
+        });
+        dialog_fontchooser.show();
+
+
+
+
         wv_formviewer  = (WebView) findViewById(R.id.submission_formloader);
         wv_formviewer.getSettings().setJavaScriptEnabled(true);
+
+        //Unicode Form
         wv_formviewer.loadUrl("https://docs.google.com/forms/d/1R_YaDOa9goBpfn6lpvx9HezOCK9kNXo_gLyprT_GVlE/viewform?usp=send_form");
+
+        //Zaw Gyi Form
+
+
 
         setSupportActionBar(toolbar);
 
@@ -47,9 +111,9 @@ public class SubmissionActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-    @Override
+   /* @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
-    }
+    }*/
 
 }
--- app/src/main/java/ui/fragments/ArticleTwoFragment.java
+++ app/src/main/java/ui/fragments/ArticleTwoFragment.java
@@ -3,6 +3,8 @@ package ui.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,26 +20,56 @@ import java.util.List;
 
 import base.BaseFragment;
 import config.GLOBAL_STRING;
+import models.Article;
 import ui.Adapters.ArticleGridAdapter;
+import ui.Adapters.ArticleListAdapter;
 import ui.activities.DefinitionActivity;
 
 /**
  * Created by winhtaikaung on 11/4/15.
  */
 public class ArticleTwoFragment extends BaseFragment {
-    GridView gv_article_pt_two;
-    ArticleGridAdapter mArticleGridAdapter;
-    List<String> article_pt_one;
+    RecyclerView gv_article_pt_two;
+    ArticleListAdapter mArticleListAdapter;
+    List<String> article_pt_two;
+    List<String> article_definition_pt_two;
+    LinearLayoutManager layoutManager;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_articleparttwo,container,false);
-        gv_article_pt_two=(GridView) v.findViewById(R.id.gv_article_pt_two);
-        article_pt_one=new ArrayList<>(Arrays.asList(getActivity().getResources().getStringArray(R.array.arr_article_title)));
-        mArticleGridAdapter=new ArticleGridAdapter(getActivity(),article_pt_one.subList(15, 30));
-        gv_article_pt_two.setAdapter(mArticleGridAdapter);
+        gv_article_pt_two=(RecyclerView) v.findViewById(R.id.gv_article_pt_two);
+        article_pt_two=new ArrayList<>(Arrays.asList(getActivity().getResources().getStringArray(R.array.arr_article_title)));
+        article_definition_pt_two=new ArrayList<>(Arrays.asList(getActivity().getResources().getStringArray(R.array.arr_article_def)));
 
-        gv_article_pt_two.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        gv_article_pt_two.setLayoutManager(layoutManager);
+
+        int arrsize=article_definition_pt_two.subList(15, 30).size();
+        Article[] articles=new Article[arrsize];
+
+        for(int i=0;i<article_definition_pt_two.subList(15, 30).size();i++){
+            Article a=new Article();
+            a.setArticledefinition(article_definition_pt_two.subList(15, 30).get(i));
+            a.setArticletitle(article_pt_two.subList(15, 30).get(i));
+            articles[i]=a;
+
+        }
+
+        mArticleListAdapter=new ArticleListAdapter(getActivity(), articles, new ArticleListAdapter.ArticleItemclickHandler() {
+            @Override
+            public void onItemClickListener(int position, Article selectedArticle, ArticleListAdapter.ArticleViewHolder vh) {
+                Intent intent=new Intent(getActivity(),DefinitionActivity.class);
+                intent.putExtra(GLOBAL_STRING.ARRAY_INDEX,position);
+                intent.putExtra(GLOBAL_STRING.ARTICLE_PART,"2");
+                startActivity(intent);
+            }
+        });
+        gv_article_pt_two.setAdapter(mArticleListAdapter);
+
+        /*gv_article_pt_two.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Log.i("GRID_POSITION", String.valueOf(view.getTag()));
@@ -46,7 +78,7 @@ public class ArticleTwoFragment extends BaseFragment {
                 intent.putExtra(GLOBAL_STRING.ARTICLE_PART,"2");
                 startActivity(intent);
             }
-        });
+        });*/
         return v;
     }
 }
